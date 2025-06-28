12c79e05ef7087c98e8f156beab16ef6178e9d63
FLNTitan <osh.kalfon@gmail.com>
2025-06-28 12:52:30 +0300

    fix font path

--- modules/thumbnail_config.py
+++ modules/thumbnail_config.py
@@ -1,6 +1,6 @@
 THUMBNAIL_STYLES = {
     "default": {
-        "font_path": "../assets/static/Roboto-Bold.ttf",
+        "font_path": "assets/static/Roboto-Bold.ttf",
         "font_size": 48,
         "font_color": (255, 255, 255),
         "background_color": (0, 0, 0, 128),  # semi-transparent overlay behind text

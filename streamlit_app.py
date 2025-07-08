5ecb503931764c8fc0907e2cc3e7eb82268a01f6
FLNTitan <95741435+FLNTitan@users.noreply.github.com>
2025-07-08 10:24:21 +0300

    add rerun

--- streamlit_app.py
+++ streamlit_app.py
@@ -402,6 +402,7 @@ if section == "🧠 Content Ideas":
                                     + st.session_state["auri_steps"][insertion_index:]
                                 )
                                 st.session_state["auri_context"]["video_workflow"] = workflow
+                                st.experimental_rerun()
                         elif "caption" in title or "hashtag" in title:
                             from modules.captions import generate_caption
                             from modules.hashtags import generate_hashtags
